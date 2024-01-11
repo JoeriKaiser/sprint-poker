c519e4b230cf48af30a8879cf7507e339b707bc1
Joeri <joeri.kaiser@hotmail.com>
2024-01-11 19:39:04 +0100

    Fix traits and imports

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,12 +1,14 @@
+use uuid::Uuid;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct User {
     pub uuid:Uuid,
     pub username: String,
-    pub created_at: Timestamp,
+    // TODO fix timestamp
+    // pub created_at: Timestamp,
 }
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
-pub struct createUser {
+pub struct CreateUser {
     pub username: String,
 }
