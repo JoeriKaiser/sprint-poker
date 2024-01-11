eaf9f80f13d3c09aba4d3f71d4e29c1864c22040
Joeri <joeri.kaiser@hotmail.com>
2024-01-11 19:03:24 +0100

    Add users endpoitns

---
+++ api/lib/src/users.rs
@@ -0,0 +1,35 @@
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpResponse,
+};
+
+pub fn service(cfg: &mut ServiceConfig) {
+    cfg.service(
+        web::scope("v1/users")
+            .route("", web::get().to(get_all))
+            .route("/{user_id}", web::get().to(get))
+            .route("", web::post().to(post))
+            .route("", web::post().to(put))
+            .route("/user_id", web::delete().to(delete))
+    );
+}
+
+async fn get_all() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn get() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn post() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn put() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn delete() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
