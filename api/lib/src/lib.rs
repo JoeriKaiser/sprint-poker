a1bc929fab3f242e751270522059e333c83ac9fe
Joeri <joeri.kaiser@hotmail.com>
2024-01-11 19:36:39 +0100

    Add user repository trait

--- api/lib/src/lib.rs
+++ api/lib/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod health;
 pub mod users;
+
+pub mod user_repository;
---
+++ api/lib/src/user_repository/mod.rs
@@ -0,0 +1,14 @@
+use shared::models::{CreateUser, User};
+use uuid::Uuid;
+
+pub type UserError = String;
+pub type UserResult<T> = Result<T, UserError>;
+
+#[async_trait::async_trait]
+pub trait UserRepository: Send + Sync + 'static {
+    async fn get_users(&self) -> UserResult<Vec<User>>;
+    async fn get_user(&self, id: &Uuid) -> UserResult<User>;
+    async fn create_user(&self, id: &CreateUser) -> UserResult<User>;
+    async fn update_user(&self, id: &User) -> UserResult<User>;
+    async fn delete_user(&self, id: &Uuid) -> UserResult<User>;
+}
