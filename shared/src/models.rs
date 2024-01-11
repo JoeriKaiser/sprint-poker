c7e705d51668d7b3c0f41cfed7c3417f1e4e0314
Joeri <joeri.kaiser@hotmail.com>
2024-01-11 19:22:25 +0100

    Add serde for serializing and deserializing. Also added derive traits

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,9 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct User {
     pub uuid:Uuid,
     pub username: String,
     pub created_at: Timestamp,
 }
-
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct createUser {
     pub username: String,
 }
