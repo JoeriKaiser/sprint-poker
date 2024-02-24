2c3134c91c94bbe779ffb72e499be83f79e32acd
Joeri <joeri.kaiser@hotmail.com>
2024-02-25 00:46:03 +0100

    Update model structs to use Serialize and Deserialize

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,6 +1,8 @@
 use chrono::format::Numeric;
 use uuid::Uuid;
 use serde::{Deserialize, Serialize};
+
+#[cfg(feature = "backend")]
 use sqlx::FromRow;
 
 #[cfg_attr(feature = "backend", derive(FromRow))]
@@ -11,18 +13,21 @@ pub struct User {
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Session {
     pub uuid: Uuid,
     pub session_name: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
 }
-
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Ticket {
     pub uuid: Uuid,
     pub username: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Vote {
     pub uuid: Uuid,
     pub value: String,
@@ -35,10 +40,12 @@ pub struct CreateUser {
     pub username: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct CreateSession {
     pub session_name: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct CreateTicket {
     pub ticket_name: String,
 }
